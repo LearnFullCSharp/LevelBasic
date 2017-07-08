0f5f4b1a8c8f2d894ef8b67192baa49e32fed154
jesuscanales <jesuscanales@outlook.com>
2017-07-08 01:38:33 -0500

    StructureIf

---
+++ Lab07/OperatorIf/OperatorIf/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorIf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int edad = 55;
+            if (edad < 30) {
+                //Código si es cierto
+                Console.WriteLine("Mensaje de verdad");
+                Console.ReadKey();
+            }
+            else {
+                //Código si es falso
+                Console.WriteLine("Mensaje de falsedad");
+                Console.ReadKey();
+            };
+        }
+    }
+}
