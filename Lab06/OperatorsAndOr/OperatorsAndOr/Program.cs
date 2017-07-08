d3e8b153ea0f5c084304f611539010c3a3a47121
jesuscanales <jesuscanales@outlook.com>
2017-07-08 00:36:58 -0500

    Operator And Or

---
+++ Lab06/OperatorsAndOr/OperatorsAndOr/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsAndOr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Operador And
+            Console.WriteLine("Es cierto que: " + (3 == 3 && 4 == 4));
+            Console.WriteLine("Es cierto que: " + (3 == 3 && 4 == 5));
+
+            //Operador Or
+            Console.WriteLine("Es cierto que: " + (3 == 3 || 4 == 6));
+            Console.WriteLine("Es cierto que: " + (3 > 1 || 2 == 2));
+            Console.ReadKey();
+        }
+    }
+}
