d6f19653bea92514b77db01a303cff2a65e06d83
jesuscanales <jesuscanales@outlook.com>
2017-07-08 00:17:38 -0500

    OperatorsComparison

--- Lab05/ComparisonOperator/ComparisonOperator/Program.cs
+++ Lab05/ComparisonOperator/ComparisonOperator/Program.cs
@@ -10,6 +10,14 @@ namespace ComparisonOperator
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Es cierto que tres es igual a tres?: " + (3 == 3));
+            Console.WriteLine("Es cierto que tres es diferente que tres?: " + (3 != 3));
+            Console.WriteLine("Es cierto que tres es mayor que tres?: " + (3 > 3));
+            Console.WriteLine("Es cierto que tres es menor que traes?: " + (3 < 3));
+            Console.WriteLine("Es cierto que tres es mayor e igual que tres?: " + (3 >= 3));
+            Console.WriteLine("Es cierto que tres es menor igual que tres?: " + (3 <= 3));
+
+            Console.ReadKey();
         }
     }
 }
