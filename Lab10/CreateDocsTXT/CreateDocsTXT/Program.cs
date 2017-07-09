c630a2d89e42fa969effbe5a9a740e577a4c77eb
jesuscanales <jesuscanales@outlook.com>
2017-07-09 11:58:18 -0500

    CreateAndRead_DocsTXT

---
+++ Lab10/CreateDocsTXT/CreateDocsTXT/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO; //Permite asignar las líneas para permitir habilitar el TextWriter
+
+namespace CreateDocsTXT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TextWriter texto = new StreamWriter("prueba.txt"); //Permite crear un archivo de texto en blanco
+            texto.WriteLine("Hello World; my name is Jesús"); //Crea una línea de texto en el documento TXT
+            texto.Close(); //Termina el uso del documento TXT
+
+        }
+    }
+}
---
+++ Lab10/ReadDocsTXT/ReadDocsTXT/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ReadDocsTXT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Este código debe ir acompañado con el proyecto de crear documentos TXT
+            TextReader texto2 = new StreamReader("prueba.txt"); //Abre el archivo de texto
+            Console.WriteLine(texto2.ReadLine()); //Imprime en consola el contenido del archivo de texto
+            texto2.Close(); //Cerrar el documento
+
+            Console.ReadLine(); //Esperar una tecla para cerrar la ventana de consola
+        }
+    }
+}
