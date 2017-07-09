9ce48492955d9cc9439e7293908dba348452a4ac
jesuscanales <jesuscanales@outlook.com>
2017-07-09 10:32:27 -0500

    Working of matriz

---
+++ Lab09/Matriz/Matriz/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matriz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nombre = new string[100];
+            nombre[0] = "José";
+            nombre[1] = "Juan";
+            nombre[2] = "Jorge";
+            nombre[3] = "Julian";
+            nombre[4] = "Javier";
+            nombre[5] = "Jaime";
+
+            for (int elementomatriz = 0; elementomatriz < 10; elementomatriz++) {
+                Console.WriteLine(nombre[elementomatriz]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
