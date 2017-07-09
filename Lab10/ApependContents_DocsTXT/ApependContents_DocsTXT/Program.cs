3a5c404cf507a82aaa9bf649c9cc78cd27cf1992
jesuscanales <jesuscanales@outlook.com>
2017-07-09 13:07:39 -0500

    Append new lines in documents

---
+++ Lab10/ApependContents_DocsTXT/ApependContents_DocsTXT/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ApependContents_DocsTXT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Este código es complemento de los proyectos anteriores ya que todos direccionan al primer proyecto
+            StreamWriter texto = File.AppendText("prueba.txt");
+            texto.WriteLine("Nuevo texto agregado");
+            texto.Close();
+        }
+    }
+}
