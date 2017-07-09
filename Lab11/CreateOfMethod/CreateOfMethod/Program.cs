2547140828c7b52e8a28f4a8920c253819f35d63
jesuscanales <jesuscanales@outlook.com>
2017-07-09 17:24:18 -0500

    Working whit Method

---
+++ Lab11/CreateOfMethod/CreateOfMethod/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateOfMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            mimetodo("Jesús");
+            mimetodo("Jaime");
+            mimetodo("Jorge", "25");
+            Console.ReadKey();
+        }
+
+        static void mimetodo(string nombre)
+        {
+            Console.WriteLine("Escribo texto desde el método, el nombre es: " + nombre);
+        }
+
+        static void mimetodo(string nombre, string edad)
+        {
+            Console.WriteLine("Escribo texto desde el método, el nombre es: " + nombre + " y mi edad es " + edad + " años.");
+        }
+    }
+}
