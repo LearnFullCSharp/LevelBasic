a70052e9d67e4933195bd79c4b8c004d3de816e1
jesuscanales <jesuscanales@outlook.com>
2017-07-10 09:35:39 -0500

    Working with class

---
+++ Lab12/WorkingWithClass/WorkingWithClass/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkingWithClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Perro tobi = new Perro();
+            tobi.color = "marrón";
+            tobi.edad = 15;
+            Console.WriteLine("Tobi tiene " + tobi.edad + " años y es de color " + tobi.color);
+            tobi.ladra();
+            Console.ReadLine();
+        }
+    }
+
+    class Perro
+    {
+        public string color;
+        public int edad;
+
+        public void ladra()
+        {
+            Console.WriteLine("Guauu");
+        }
+
+        public void duerme()
+        {
+            Console.WriteLine("Zzzzzz");
+        }
+    }
+}
